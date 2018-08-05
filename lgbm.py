c219759f93b52c217811730fbdf286044923dc99
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-08-05 16:01:40 +0900

    [fix]実行できないバグを修正

--- lgbm.py
+++ lgbm.py
@@ -6,12 +6,21 @@ from contextlib import contextmanager
 from lightgbm import LGBMClassifier
 from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.model_selection import KFold, StratifiedKFold
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import tqdm
 
 
+def load_data(feats):
+    f = feats.pop()
+    df = pd.read_feather(f'./data/{f}_df.ftr')
+    for f in feats:
+        df = pd.merge(df,pd.read_feather(f'./data/{f}_df.ftr'),how="left",on="SK_ID_CURR")
+    return df
+
 def main():
-    
+    feats = ["application","bureau_and_balance"]
+    df = load_data(feats)
+
+if __name__ == "__main__":
+
+    main()
