d38705c8c3625a8be350dc614b4bb340d7900093
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-08-05 22:09:23 +0900

    [add]複数class追加

--- features.py
+++ features.py
@@ -32,7 +32,6 @@ class application(Feature):
         df['INCOME_PER_PERSON'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS'] # 総収入/家族人数
         df['ANNUITY_INCOME_PERC'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL'] # 月々の返済額/総収入
         df['PAYMENT_RATE'] = df['AMT_ANNUITY'] / df['AMT_CREDIT'] # 月々の返済額/借入額
-
         self.df = df.reset_index()
    
         del (df);gc.collect()
@@ -98,10 +97,125 @@ class bureau_and_balance(Feature):
         bureau_agg = pd.merge(bureau_agg,closed_agg, how='left', on='SK_ID_CURR')
         del closed, closed_agg, bureau
         gc.collect()
-        bureau_agg.to_csv("tes.csv")
         self.df = bureau_agg
 
         del bureau_agg;gc.collect()
+
+class previous_aplications(Feature):
+    def create_feature(self):
+        prev = pd.read_csv('./input/previous_application.csv', nrows = num_rows)
+        prev, cat_cols = one_hot_encoder(prev, nan_as_category= True)
+        # Days 365.243 values -> nan
+        prev['DAYS_FIRST_DRAWING'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_FIRST_DUE'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_LAST_DUE_1ST_VERSION'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_LAST_DUE'].replace(365243, np.nan, inplace= True)
+        prev['DAYS_TERMINATION'].replace(365243, np.nan, inplace= True)
+        # Add feature: value ask / value received percentage
+        prev['APP_CREDIT_PERC'] = prev['AMT_APPLICATION'] / prev['AMT_CREDIT']
+        # Previous applications numeric features
+        num_aggregations = {
+            'AMT_ANNUITY': [ 'max', 'mean'],
+            'AMT_APPLICATION': [ 'max','mean'],
+            'AMT_CREDIT': [ 'max', 'mean'],
+            'APP_CREDIT_PERC': [ 'max', 'mean'],
+            'AMT_DOWN_PAYMENT': [ 'max', 'mean'],
+            'AMT_GOODS_PRICE': [ 'max', 'mean'],
+            'HOUR_APPR_PROCESS_START': [ 'max', 'mean'],
+            'RATE_DOWN_PAYMENT': [ 'max', 'mean'],
+            'DAYS_DECISION': [ 'max', 'mean'],
+            'CNT_PAYMENT': ['mean', 'sum'],
+        }
+        # Previous applications categorical features
+        cat_aggregations = {}
+        for cat in cat_cols:
+            cat_aggregations[cat] = ['mean']
+    
+        prev_agg = prev.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+        prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+        #Previous Applications: Approved Applications - only numerical features
+        approved = prev[prev['NAME_CONTRACT_STATUS_Approved'] == 1]
+        approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
+        approved_agg.columns = pd.Index(['APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns.tolist()])
+        prev_agg = prev_agg.join(approved_agg, how='left', on='SK_ID_CURR')
+        # Previous Applications: Refused Applications - only numerical features
+        refused = prev[prev['NAME_CONTRACT_STATUS_Refused'] == 1]
+        refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
+        refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
+        prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
+        del refused, refused_agg, approved, approved_agg, prev
+        gc.collect()
+        
+        self.df = prev_agg
+
+class pos_cash(Feature):
+    def create_features(self,nan_as_category=True):
+        pos = pd.read_csv('./input/POS_CASH_balance.csv', nrows = num_rows)
+        pos, cat_cols = one_hot_encoder(pos, nan_as_category= True)
+        
+        aggregations = {
+            'MONTHS_BALANCE': ['max', 'mean', 'size'],
+            'SK_DPD': ['max', 'mean'],
+            'SK_DPD_DEF': ['max', 'mean']
+        }
+        for cat in cat_cols:
+            aggregations[cat] = ['mean']
+    
+        pos_agg = pos.groupby('SK_ID_CURR').agg(aggregations)
+        pos_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in pos_agg.columns.tolist()])
+        # Count pos cash accounts
+        pos_agg['POS_COUNT'] = pos.groupby('SK_ID_CURR').size()
+        del pos
+        gc.collect()
+        self.df = pos_agg
+
+class installments_payments(Feature):
+    def create_features(self,nan_as_category=True):
+        ins = pd.read_csv('./input/installments_payments.csv', nrows = num_rows)
+        ins, cat_cols = one_hot_encoder(ins, nan_as_category= True)
+        # Percentage and difference paid in each installment (amount paid and installment value)
+        ins['PAYMENT_PERC'] = ins['AMT_PAYMENT'] / ins['AMT_INSTALMENT']
+        ins['PAYMENT_DIFF'] = ins['AMT_INSTALMENT'] - ins['AMT_PAYMENT']
+        # Days past due and days before due (no negative values)
+        ins['DPD'] = ins['DAYS_ENTRY_PAYMENT'] - ins['DAYS_INSTALMENT']
+        ins['DBD'] = ins['DAYS_INSTALMENT'] - ins['DAYS_ENTRY_PAYMENT']
+        ins['DPD'] = ins['DPD'].apply(lambda x: x if x > 0 else 0)
+        ins['DBD'] = ins['DBD'].apply(lambda x: x if x > 0 else 0)
+        # Features: Perform aggregations
+        aggregations = {
+            'NUM_INSTALMENT_VERSION': ['nunique'],
+            'DPD': ['max', 'mean', 'sum','min','std' ],
+            'DBD': ['max', 'mean', 'sum','min','std'],
+            'PAYMENT_PERC': [ 'max','mean',  'var','min','std'],
+            'PAYMENT_DIFF': [ 'max','mean', 'var','min','std'],
+            'AMT_INSTALMENT': ['max', 'mean', 'sum','min','std'],
+            'AMT_PAYMENT': ['min', 'max', 'mean', 'sum','std'],
+            'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum','std']
+        }
+        for cat in cat_cols:
+            aggregations[cat] = ['mean']
+        ins_agg = ins.groupby('SK_ID_CURR').agg(aggregations)
+        ins_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in ins_agg.columns.tolist()])
+        # Count installments accounts
+        ins_agg['INSTAL_COUNT'] = ins.groupby('SK_ID_CURR').size()
+        del ins
+        gc.collect()
+        self.df = ins_agg
+
+class credit_card_valance(Feature):
+    def create_features(self,num_rows = None, nan_as_category = True):
+        cc = pd.read_csv('../input/credit_card_balance.csv', nrows = num_rows)
+        cc, cat_cols = one_hot_encoder(cc, nan_as_category= True)
+        # General aggregations
+        cc.drop(['SK_ID_PREV'], axis= 1, inplace = True)
+        cc_agg = cc.groupby('SK_ID_CURR').agg([ 'max', 'mean', 'sum', 'var'])
+        cc_agg.columns = pd.Index(['CC_' + e[0] + "_" + e[1].upper() for e in cc_agg.columns.tolist()])
+        # Count credit card lines
+        cc_agg['CC_COUNT'] = cc.groupby('SK_ID_CURR').size()
+        del cc
+        gc.collect()
+        self.df = cc_agg
+
 
 if __name__ == '__main__':
     args = get_arguments()
