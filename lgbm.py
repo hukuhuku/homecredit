112335f8c215adfadf4b99aee1b923f16c6c743e
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-08-05 22:08:59 +0900

    [add]学習回せるようにした

--- lgbm.py
+++ lgbm.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import numpy as np
 import pandas as pd
 import gc
@@ -7,20 +10,93 @@ from lightgbm import LGBMClassifier
 from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.model_selection import KFold, StratifiedKFold
 
+from base import *
 import tqdm
 
 
-def load_data(feats):
+
+def load_data(feats,debug=False):
     f = feats.pop()
     df = pd.read_feather(f'./data/{f}_df.ftr')
+    if debug:
+        df = df.loc[:10000]
     for f in feats:
         df = pd.merge(df,pd.read_feather(f'./data/{f}_df.ftr'),how="left",on="SK_ID_CURR")
-    return df
+    return df                                                         
+
+
+def fit_predict(df, num_folds, stratified = False, debug= False):
+    # Divide in training/validation and test data
+    train_df = df[df['TARGET'].notnull()]
+    test_df = df[df['TARGET'].isnull()]
+    print("Starting LightGBM. Train shape: {}, test shape: {}".format(train_df.shape, test_df.shape))
+    del df
+    gc.collect()
+    # Cross validation model
+    if stratified:
+        folds = StratifiedKFold(n_splits= num_folds, shuffle=True, random_state=47)
+    else:
+        folds = KFold(n_splits= num_folds, shuffle=True, random_state=47)
+    # Create arrays and dataframes to store results
+    oof_preds = np.zeros(train_df.shape[0])
+    sub_preds = np.zeros(test_df.shape[0])
+    feature_importance_df = pd.DataFrame()
+    feats = [f for f in train_df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+    
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['TARGET'])):
+        train_x, train_y = train_df[feats].iloc[train_idx], train_df['TARGET'].iloc[train_idx]
+        valid_x, valid_y = train_df[feats].iloc[valid_idx], train_df['TARGET'].iloc[valid_idx]
+
+        # LightGBM parameters found by Bayesian optimization
+        clf = LGBMClassifier(
+            n_jobs = 4,
+            #is_unbalance=True,
+            n_estimators=10000,
+            learning_rate=0.02,
+            num_leaves=32,
+            colsample_bytree=0.9497036,
+            subsample=0.8715623,
+            max_depth=8,
+            reg_alpha=0.04,
+            reg_lambda=0.073,
+            min_split_gain=0.0222415,
+            min_child_weight=40,
+            silent=-1,
+            verbose=-1,
+            #scale_pos_weight=11
+            )
+
+        clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)], 
+            eval_metric= 'auc', verbose= 1000, early_stopping_rounds= 200)
+
+        oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
+        sub_preds += clf.predict_proba(test_df[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
+
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["feature"] = feats
+        fold_importance_df["importance"] = clf.feature_importances_
+        fold_importance_df["fold"] = n_fold + 1
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+        print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx])))
+        del clf, train_x, train_y, valid_x, valid_y
+        gc.collect()
+
+    print('Full AUC score %.6f' % roc_auc_score(train_df['TARGET'], oof_preds))
+    # Write submission file and plot feature importance
+
+    feature_importance_df.to_csv("feature_importance_df.csv",index=True,header=True)
+    if not debug:
+        test_df['TARGET'] = sub_preds
+        test_df[['SK_ID_CURR', 'TARGET']].to_csv(submission_file_name, index= False)
 
 def main():
     feats = ["application","bureau_and_balance"]
-    df = load_data(feats)
+    debug = True
+    df = load_data(feats,debug=debug)
+    with timer("Run LightGBM with kfold"):
+        fit_predict(df, num_folds= 5, stratified= False,debug=debug)
+
 
 if __name__ == "__main__":
-
+    submission_file_name = "submission_kernel02.csv"
     main()
