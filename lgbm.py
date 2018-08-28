5818c67cb7a10d14d9ddab13821d608c630b7b9a
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-08-28 16:55:53 +0900

    [add]GridSearch追記

--- lgbm.py
+++ lgbm.py
@@ -10,31 +10,16 @@ from lightgbm import LGBMClassifier
 from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import (roc_curve, auc, accuracy_score)
+from sklearn.model_selection import GridSearchCV
 
 from base import *
 import tqdm
 
-def get_clf():
-    # LightGBM parameters found by Bayesian optimization
-    return LGBMClassifier(
-            n_jobs = -1,
-            #is_unbalance=True,
-            n_estimators=10000,
-            learning_rate=0.02,
-            num_leaves=32,
-            colsample_bytree=0.9497036,
-            subsample=0.8715623,
-            max_depth=8,
-            reg_alpha=0.04,
-            reg_lambda=0.073,
-            min_split_gain=0.0222415,
-            min_child_weight=40,
-            silent=-1,
-            verbose=-1,
-            #scale_pos_weight=11
-            )
+    
 
-def fit_predict(train,test,num_folds,debug,name):
+
+def fit_predict(train,test,num_folds,debug):
     models = []
     print("Starting LightGBM. Train shape: {}, test shape: {}".format(train.shape, test.shape))
     gc.collect()
@@ -48,15 +33,35 @@ def fit_predict(train,test,num_folds,debug,name):
     fold_importance_df = pd.DataFrame()
     fold_importance_df["feature"] = feats
     fold_importance_df["importance"] = 0
-
     for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train[feats], train['TARGET'])):
         train_x, train_y = train[feats].iloc[train_idx], train['TARGET'].iloc[train_idx]
         valid_x, valid_y = train[feats].iloc[valid_idx], train['TARGET'].iloc[valid_idx]
 
-        clf = get_clf()
+        lgb_c = LGBMClassifier(
+            n_jobs = -1,
+            #is_unbalance=True,
+            n_estimators=10000,
+            #learning_rate=[0.1, 0.05, 0.02, 0.01],
+            #num_leaves=[32,34,36,38],
+            colsample_bytree=0.9497036,
+            subsample=0.8715623,
+            #max_depth=[4,6,8],
+            reg_alpha=0.041545473,
+            reg_lambda=0.0735294,
+            min_split_gain=0.0222415,
+            min_child_weight=39.3259775,
+            silent=-1,
+            verbose=-1, 
+        )
+        grid_params = {
+            "learning_rate":[0.1, 0.05, 0.02, 0.01],
+            "num_leaves":[32,34,36,38],"max_depth":[4,6,8]
 
-        clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)], 
-            eval_metric= 'auc', verbose= 100, early_stopping_rounds= 200)
+        }
+
+        
+        clf = GridSearchCV(lgb_c,grid_params,cv = 2, verbose= 3)
+        clf.fit(train_x, train_y)
 
         oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
         sub_preds += clf.predict_proba(test[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
@@ -70,24 +75,9 @@ def fit_predict(train,test,num_folds,debug,name):
 
     #if not debug:
     test['TARGET'] = sub_preds
-    test[['SK_ID_CURR', 'TARGET']].to_csv("./output/"+name+"_lgbm_{}.csv".format(score), index= False)
+    test[['SK_ID_CURR', 'TARGET']].to_csv("./output/"+name+"_lgbm.csv", index= False)
 
     return fold_importance_df.sort_values("importance",ascending=False)
-
-def get_del_columns(df):
-    #get no importance columns
-    feats =  [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
-    train_x, valid_x, train_y, valid_y = train_test_split(df[feats], df["TARGET"], random_state=0)
-    
-    clf = get_clf()
-    clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)], 
-        eval_metric= 'auc', verbose= 100, early_stopping_rounds= 200)
-
-    feature_importance_df = pd.DataFrame()
-    feature_importance_df["feature"] = feats
-    feature_importance_df["importance"] = clf.feature_importances_
-
-    return list(feature_importance_df.sort_values('importance', ascending=False)[-200:]["feature"].values)
 
 
   
@@ -95,26 +85,19 @@ def main(debug):
     
     feats = ["application","bureau_and_balance","pos_cash",
             "installments_payments","credit_card_valance",
-            "external_score_statics","previous_aplications",
-            "mean_perc","income"]
+            "previous_aplications","corresponding_aggregate"]
 
     name = "_and_".join(feats)
     print(name+" modeling start")
 
     with timer("Load_data"):
         train,test = get_input(feats,converting=True,debug=debug)
-    print(train.shape)
-    with timer("Run LithgGBM"):
-        del_columns = get_del_columns(train.loc[:100000])
-    
-    print(del_columns)
-    train = train.drop(del_columns,axis=1)   
-    test = test.drop(del_columns,axis=1)
+
 
     with timer("Run LightGBM with kfold"):
-        importance = fit_predict(train,test, num_folds= 3,debug=debug,name=name)
+        importance = fit_predict(train,test,debug=debug,num_folds=3)
 
-    importance.to_csv("importance.csv")
+    importance.to_csv("importance_{}.csv".format(name))
     
 if __name__ == "__main__":
     main(debug=False)
