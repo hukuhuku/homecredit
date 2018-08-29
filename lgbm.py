f4d0cf620ab24dda86d8f07a53095dcb61b51a6a
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-08-29 17:00:05 +0900

    [add]del_columns追加”

--- lgbm.py
+++ lgbm.py
@@ -35,6 +35,45 @@ def get_clf():
             verbose=-1, 
         )
     
+def get_drop_columns(train_df,test_df):
+    # Divide in training/validation and test data
+    print("Starting LightGBM. Train shape: {}".format(train_df.shape,))
+
+    feats = [f for f in train_df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+    
+    sub = test_df[["SK_ID_CURR"]]
+    target = train_df["TARGET"]
+    train_df = train_df.drop(['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index'],axis=1)
+    test_df = test_df.drop(['SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index'],axis=1)
+
+    train_x, valid_x, train_y, valid_y = train_test_split(train_df, target, random_state=0)
+    print(train_x.shape)
+    clf = get_clf()
+
+    clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)], 
+        eval_metric= 'auc', verbose= 100, early_stopping_rounds= 200)
+
+    oof_preds = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
+
+    print(len(clf.feature_importances_))
+    print(len(feats))
+    fold_importance_df = pd.DataFrame()
+    fold_importance_df["feature"] = feats
+    fold_importance_df["importance"] = clf.feature_importances_
+  
+    gc.collect()
+    score = roc_auc_score(valid_y, oof_preds)
+    print('Full AUC score %.6f' %score)
+
+    drop_columns = fold_importance_df.loc[fold_importance_df["importance"] ==0 ,"feature"]
+
+    sub["TARGET"] = clf.predict_proba(test_df, num_iteration=clf.best_iteration_)[:, 1]
+    sub[["SK_ID_CURR","TARGET"]].to_csv("./output/lgbm1.csv", index= False)
+
+    del clf, train_x, train_y, valid_x, valid_y;gc.collect()
+    return drop_columns
+
+
 def fit_predict(train,test,num_folds,debug):
     models = []
     print("Starting LightGBM. Train shape: {}, test shape: {}".format(train.shape, test.shape))
@@ -89,7 +128,11 @@ def main(debug):
     with timer("Load_data"):
         train,test = get_input(feats,converting=True,debug=debug)
 
-
+    with timer("Get Drop Columns"):
+        drop_columns = get_drop_columns(train,test)
+        train = train.drop(drop_columns,axis=1)
+        test = test.drop(drop_columns,axis=1)
+    
     with timer("Run LightGBM with kfold"):
         fit_predict(train,test,debug=debug,num_folds=3)
 
